e5e5d6d78b98e045d6f8f0a3a639cfbfcf1a88fc
Skyne <skyne9@gmail.com>
2015-03-07 09:50:37 +0100

    basic voxel map engine

---
+++ ProjectU/Assets/Scripts/Voxel/Chunk.cs
@@ -0,0 +1,224 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Chunk : MonoBehaviour
+{
+    public GameObject worldGO;
+    public int chunkX;
+    public int chunkY;
+    public int chunkZ;
+    public int chunkSize = 16;
+    public bool updateNeeded;
+
+    private WorldGen worldGen;
+    private List<Vector3> newVertices = new List<Vector3>();
+    private List<int> newTriangles = new List<int>();
+    private List<Vector2> newUV = new List<Vector2>();
+
+    private float tUnit = 0.25f;
+    private Vector2 tStone = new Vector2(1, 0);
+    private Vector2 tGrass = new Vector2(0, 1);
+    private Vector2 tGrassTop = new Vector2(1, 1);
+
+    private Mesh mesh;
+    private MeshCollider col;
+
+    private int faceCount;
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        col = GetComponent<MeshCollider>();
+
+        worldGen = worldGO.GetComponent<WorldGen>();
+
+        GenerateMesh();
+    }
+
+    void CreateCube(int x, int y, int z, byte type)
+    {
+        CubeTop(x, y, z, type);
+        CubeNorth(x, y, z, type);
+        CubeEast(x, y, z, type);
+        CubeSouth(x, y, z, type);
+        CubeWest(x, y, z, type);
+        CubeBot(x, y, z, type);
+    }
+
+    Vector2 GetTexturePos(int type, bool top)
+    {
+        if (type == 2)
+        {
+            if (top)
+                return tGrassTop;
+
+            return tGrass;
+        }
+        return tStone;
+    }
+
+    void CubeTop(int x, int y, int z, byte block)
+    {
+        newVertices.Add(new Vector3(x, y, z + 1));
+        newVertices.Add(new Vector3(x + 1, y, z + 1));
+        newVertices.Add(new Vector3(x + 1, y, z));
+        newVertices.Add(new Vector3(x, y, z));
+
+        Cube(block, true);
+    }
+
+    void CubeNorth(int x, int y, int z, byte block)
+    {
+        newVertices.Add(new Vector3(x + 1, y - 1, z + 1));
+        newVertices.Add(new Vector3(x + 1, y, z + 1));
+        newVertices.Add(new Vector3(x, y, z + 1));
+        newVertices.Add(new Vector3(x, y - 1, z + 1));
+
+        Cube(block);
+    }
+    void CubeEast(int x, int y, int z, byte block)
+    {
+        newVertices.Add(new Vector3(x + 1, y - 1, z));
+        newVertices.Add(new Vector3(x + 1, y, z));
+        newVertices.Add(new Vector3(x + 1, y, z + 1));
+        newVertices.Add(new Vector3(x + 1, y - 1, z + 1));
+
+        Cube(block);
+    }
+    void CubeSouth(int x, int y, int z, byte block)
+    {
+        newVertices.Add(new Vector3(x, y - 1, z));
+        newVertices.Add(new Vector3(x, y, z));
+        newVertices.Add(new Vector3(x + 1, y, z));
+        newVertices.Add(new Vector3(x + 1, y - 1, z));
+
+        Cube(block);
+    }
+    void CubeWest(int x, int y, int z, byte block)
+    {
+        newVertices.Add(new Vector3(x, y - 1, z + 1));
+        newVertices.Add(new Vector3(x, y, z + 1));
+        newVertices.Add(new Vector3(x, y, z));
+        newVertices.Add(new Vector3(x, y - 1, z));
+
+        Cube(block);
+    }
+    void CubeBot(int x, int y, int z, byte block)
+    {
+        newVertices.Add(new Vector3(x, y - 1, z));
+        newVertices.Add(new Vector3(x + 1, y - 1, z));
+        newVertices.Add(new Vector3(x + 1, y - 1, z + 1));
+        newVertices.Add(new Vector3(x, y - 1, z + 1));
+
+        Cube(block);
+    }
+    void Cube(byte type, bool top = false)
+    {
+        newTriangles.Add(faceCount * 4); //1
+        newTriangles.Add(faceCount * 4 + 1); //2
+        newTriangles.Add(faceCount * 4 + 2); //3
+        newTriangles.Add(faceCount * 4); //1
+        newTriangles.Add(faceCount * 4 + 2); //3
+        newTriangles.Add(faceCount * 4 + 3); //4
+
+        var texturePos = GetTexturePos(type, top);
+
+        newUV.Add(new Vector2(tUnit * texturePos.x + tUnit, tUnit * texturePos.y));
+        newUV.Add(new Vector2(tUnit * texturePos.x + tUnit, tUnit * texturePos.y + tUnit));
+        newUV.Add(new Vector2(tUnit * texturePos.x, tUnit * texturePos.y + tUnit));
+        newUV.Add(new Vector2(tUnit * texturePos.x, tUnit * texturePos.y));
+
+        faceCount++; // Add this line
+    }
+
+    byte Block(int x, int y, int z)
+    {
+        return worldGen.Block(x + chunkX, y + chunkY, z + chunkZ);
+    }
+
+    public void GenerateMesh()
+    {
+        for (int x = 0; x < chunkSize; x++)
+        {
+            for (int y = 0; y < chunkSize; y++)
+            {
+                for (int z = 0; z < chunkSize; z++)
+                {
+                    //This code will run for every block in the chunk
+
+                    if (Block(x, y, z) != 0)
+                    {
+                        //If the block is solid
+
+                        if (Block(x, y + 1, z) == 0)
+                        {
+                            //Block above is air
+                            CubeTop(x, y, z, Block(x, y, z));
+                        }
+
+                        if (Block(x, y - 1, z) == 0)
+                        {
+                            //Block below is air
+                            CubeBot(x, y, z, Block(x, y, z));
+                        }
+
+                        if (Block(x + 1, y, z) == 0)
+                        {
+                            //Block east is air
+                            CubeEast(x, y, z, Block(x, y, z));
+                        }
+
+                        if (Block(x - 1, y, z) == 0)
+                        {
+                            //Block west is air
+                            CubeWest(x, y, z, Block(x, y, z));
+                        }
+
+                        if (Block(x, y, z + 1) == 0)
+                        {
+                            //Block north is air
+                            CubeNorth(x, y, z, Block(x, y, z));
+                        }
+
+                        if (Block(x, y, z - 1) == 0)
+                        {
+                            //Block south is air
+                            CubeSouth(x, y, z, Block(x, y, z));
+                        }
+                    }
+
+                }
+            }
+        }
+
+        UpdateMesh();
+    }
+
+    void LateUpdate()
+    {
+        if (updateNeeded)
+        {
+            GenerateMesh();
+            updateNeeded = false;
+        }
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = newVertices.ToArray();
+        mesh.uv = newUV.ToArray();
+        mesh.triangles = newTriangles.ToArray();
+        mesh.Optimize();
+        mesh.RecalculateNormals();
+
+        col.sharedMesh = null;
+        col.sharedMesh = mesh;
+
+        newVertices.Clear();
+        newUV.Clear();
+        newTriangles.Clear();
+
+        faceCount = 0;
+    }
+}
---
+++ ProjectU/Assets/Scripts/Voxel/ModifyTerrain.cs
@@ -0,0 +1,213 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModifyTerrain : MonoBehaviour
+{
+    private WorldGen world;
+    private GameObject cameraGO;
+    private GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        world = gameObject.GetComponent<WorldGen>();
+        cameraGO = GameObject.FindGameObjectWithTag("MainCamera");
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void LoadChunks(Vector3 playerPos, float distToLoad, float distToUnload)
+    {
+        for (int x = 0; x < world.chunks.GetLength(0); x++)
+        {
+            for (int z = 0; z < world.chunks.GetLength(2); z++)
+            {
+
+                float dist = Vector2.Distance(new Vector2(x * world.chunkSize,
+                z * world.chunkSize), new Vector2(playerPos.x, playerPos.z));
+
+                if (dist < distToLoad)
+                {
+                    if (world.chunks[x, 0, z] == null)
+                    {
+                        world.GenColumn(x, z);
+                    }
+                }
+                else if (dist > distToUnload)
+                {
+                    if (world.chunks[x, 0, z] != null)
+                    {
+                        world.UnloadColumn(x, z);
+                    }
+                }
+            }
+        }
+    }
+
+    public void ReplaceBlockCenter(float range, byte block)
+    {
+        //Replaces the block directly in front of the player
+        Ray ray = new Ray(cameraGO.transform.position, cameraGO.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+
+            if (hit.distance < range)
+            {
+                ReplaceBlockAt(hit, block);
+            }
+        }
+    }
+
+    public void AddBlockCenter(float range, byte block)
+    {
+        //Adds the block specified directly in front of the player
+        Ray ray = new Ray(cameraGO.transform.position, cameraGO.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+
+            if (hit.distance < range)
+            {
+                AddBlockAt(hit, block);
+            }
+            Debug.DrawLine(ray.origin, ray.origin + (ray.direction * hit.distance), Color.green, 2);
+        }
+    }
+
+    public void ReplaceBlockCursor(byte block)
+    {
+        //Replaces the block specified where the mouse cursor is pointing
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+
+            ReplaceBlockAt(hit, block);
+            Debug.DrawLine(ray.origin, ray.origin + (ray.direction * hit.distance),
+             Color.green, 2);
+
+        }
+    }
+
+    public void AddBlockCursor(byte block)
+    {
+        //Adds the block specified where the mouse cursor is pointing
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+
+            AddBlockAt(hit, block);
+            Debug.DrawLine(ray.origin, ray.origin + (ray.direction * hit.distance),
+             Color.green, 2);
+        }
+    }
+
+    public void ReplaceBlockAt(RaycastHit hit, byte block)
+    {
+        //removes a block at these impact coordinates, you can raycast against the terrain and call this with the hit.point
+
+        Vector3 position = hit.point;
+        position += (hit.normal * -0.5f);
+
+        SetBlockAt(position, block);
+    }
+
+    public void AddBlockAt(RaycastHit hit, byte block)
+    {
+        //adds the specified block at these impact coordinates, you can raycast against the terrain and call this with the hit.point
+        Vector3 position = hit.point;
+        position += (hit.normal * 0.5f);
+
+        SetBlockAt(position, block);
+    }
+
+    public void SetBlockAt(Vector3 position, byte block)
+    {
+        //sets the specified block at these coordinates
+
+        int x = Mathf.RoundToInt(position.x);
+        int y = Mathf.RoundToInt(position.y);
+        int z = Mathf.RoundToInt(position.z);
+
+        SetBlockAt(x, y, z, block);
+    }
+
+    public void SetBlockAt(int x, int y, int z, byte block)
+    {
+        //adds the specified block at these coordinates
+
+        print("Adding: " + x + ", " + y + ", " + z);
+
+        world.data[x, y, z] = block;
+        UpdateChunkAt(x, y, z);
+    }
+
+    //To do: add a way to just flag the chunk for update then it update it in lateupdate
+    public void UpdateChunkAt(int x, int y, int z)
+    {
+        //Updates the chunk containing this block
+
+        int updateX = Mathf.FloorToInt(x / world.chunkSize);
+        int updateY = Mathf.FloorToInt(y / world.chunkSize);
+        int updateZ = Mathf.FloorToInt(z / world.chunkSize);
+
+        print("Updating: \" + updateX + \", \" + updateY + \", \" + updateZ");
+
+        EnsureChunkUpdates(x, y, z, updateX, updateY, updateZ);
+    }
+
+    private void EnsureChunkUpdates(int x, int y, int z, int updateX, int updateY, int updateZ)
+    {
+        world.chunks[updateX, updateY, updateZ].updateNeeded = true;
+
+        if (x - (world.chunkSize * updateX) == 0 && updateX != 0)
+        {
+            world.chunks[updateX - 1, updateY, updateZ].updateNeeded = true;
+        }
+
+        if (x - (world.chunkSize * updateX) == 15 && updateX != world.chunks.GetLength(0) - 1)
+        {
+            world.chunks[updateX + 1, updateY, updateZ].updateNeeded = true;
+        }
+
+        if (y - (world.chunkSize * updateY) == 0 && updateY != 0)
+        {
+            world.chunks[updateX, updateY - 1, updateZ].updateNeeded = true;
+        }
+
+        if (y - (world.chunkSize * updateY) == 15 && updateY != world.chunks.GetLength(1) - 1)
+        {
+            world.chunks[updateX, updateY + 1, updateZ].updateNeeded = true;
+        }
+
+        if (z - (world.chunkSize * updateZ) == 0 && updateZ != 0)
+        {
+            world.chunks[updateX, updateY, updateZ - 1].updateNeeded = true;
+        }
+
+        if (z - (world.chunkSize * updateZ) == 15 && updateZ != world.chunks.GetLength(2) - 1)
+        {
+            world.chunks[updateX, updateY, updateZ + 1].updateNeeded = true;
+        }
+    }
+
+    void Update()
+    {
+        LoadChunks(player.transform.position, 32, 48);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            ReplaceBlockCursor(0);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            AddBlockCursor(1);
+        }
+    }
+}
